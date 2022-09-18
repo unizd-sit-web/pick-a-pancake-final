64ade7113b61a346777bdff0007e1f0a39f3c1b8
Daria Babić <75084311+dariababic@users.noreply.github.com>
2022-09-18 18:45:55 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,41 @@
-from flask import Flask, render_template, request
+from email.policy import default
+from wsgiref.validate import validator
+from flask import Flask,render_template, request
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///reservation.db'
+#secret key
+app.config['SECRET_KEY'] = "secret key"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+#initialize
+db = SQLAlchemy(app)
 
+
+class reservations(db.Model):
+    id=db.Column(db.Integer, primary_key=True)
+    date=db.Column(db.String(10), nullable=False)
+    table_mark=db.Column(db.String(10), nullable=False)
+    name=db.Column(db.String(60), nullable=False)
+    email=db.Column(db.String(60), nullable=False)
+
+@app.route("/reservation", methods=["POST"])
+def table_reservation():
+    name=request.form.get("name")
+    surname=request.form.get("surname")
+    date=request.form.get("date")
+    table_mark=request.form.get("table_mark")
+    email_res=request.form.get("email_res")
+    unique=False
+    return render_template("reservation.html")
+
+
+    
 @app.route('/')
 @app.route("/index/")
 def index():
@@ -38,6 +70,7 @@ def reservedform():
     quantitiy = request.form.get ("quantity")
     email = request.form.get("email")
     return render_template ('reservedform.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
